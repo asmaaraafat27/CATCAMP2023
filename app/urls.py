637e19d88dde485be05d3d09bb8052d0d7a994d0
Asmaa <asbeauty80@gmail.com>
2023-02-28 03:35:04 +0200

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('usd_to_egp/', views.usd_to_egp, name='usd-to-egp'),
-    path('get_price/', views.usd_to_egp, name='live-indomie-price'),
+    path('get_price/', views.get_price, name='live-indomie-price'),
     # pssst, yes, you'll write code here :)
 ]
